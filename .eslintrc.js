ad9ea198e873d61eb2985471d92d1ccdc94c9a79
Yelynn Oh <yoyelynn@gmail.com>
2024-01-20 15:27:39 +0900

    #2 chore: eslint rule 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,19 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'plugin:import/recommended'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
+        'newlines-between': 'always',
+      },
+    ],
+  },
 };
